a8e4bf90d01904307bbacb343633d2c4f997fd08
casiimir <cp.ciancimino@gmail.com>
2022-10-03 18:03:03 +0200

    feat: add POST works

--- app.vue
+++ app.vue
@@ -5,17 +5,44 @@
       <p>{{ item.description }}</p>
       <p>{{ item.status }}</p>
       <p>{{ item.date }}</p>
+      <a
+        :href="`https://api.notion.com/v1/oauth/authorize?client_id=d079788a-33a0-4022-bcd1-9becf52341af&redirect_uri=https%3A%2F%2Fexample.com%2Fauth%2Fnotion%2Fcallback&response_type=code`"
+        >Add to Notion</a
+      >
     </div>
+    <button @click="onPostData">Send</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Client } from "@notionhq/client";
+const config = useRuntimeConfig();
+const notion = new Client({ auth: config.notionApiKey });
+
 let notionData = ref([]);
 
 onMounted(async () => {
   const res = await $fetch("/api/notion");
   notionData.value = res;
 });
+
+const onPostData = async () => {
+  await $fetch("/api/notion", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${config.notionApiKey}`,
+      "Notion-Version": "2022-02-22",
+    },
+    body: {
+      description: "Questa è una descrizione",
+      id: "2322",
+      Date: "2022-09-21",
+      status: "online",
+      name: "il nome",
+    },
+  });
+};
 </script>
 
 <style>
--- server/api/notion.ts
+++ server/api/notion.ts
@@ -1,12 +1,100 @@
 import { Client } from "@notionhq/client";
+import { Body } from "nuxt/dist/head/runtime/components";
 
-export default async (req: Request, res: Response) => {
+export default async (req: any, res: Response) => {
   const config = useRuntimeConfig();
 
   const notion = new Client({ auth: config.notionApiKey });
 
   if (req.method === "POST") {
-    // TODO
+    console.log("POST!");
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on("end", async () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const { description, id, Date, status, name } = JSON.parse(parsedBody);
+
+      await notion.pages.create({
+        parent: {
+          database_id: config.notionDbId,
+        },
+        properties: {
+          Description: {
+            id: "%3ClzI",
+            type: "rich_text",
+            rich_text: [
+              {
+                type: "text",
+                text: {
+                  content: description,
+                  link: null,
+                },
+                annotations: {
+                  bold: false,
+                  italic: false,
+                  strikethrough: false,
+                  underline: false,
+                  code: false,
+                  color: "default",
+                },
+                plain_text: description,
+                href: null,
+              },
+            ],
+          },
+          id: {
+            id: id,
+            type: "number",
+            number: 1,
+          },
+          Date: {
+            id: "hpaF",
+            type: "date",
+            date: {
+              start: Date,
+              end: null,
+              time_zone: null,
+            },
+          },
+          Status: {
+            id: "s%60Pf",
+            type: "select",
+            select: {
+              id: "5be22627-cf96-4769-be12-ddb929c1d8be",
+              name: status,
+              color: "blue",
+            },
+          },
+          name: {
+            id: "title",
+            type: "title",
+            title: [
+              {
+                type: "text",
+                text: {
+                  content: name,
+                  link: null,
+                },
+                annotations: {
+                  bold: false,
+                  italic: false,
+                  strikethrough: false,
+                  underline: false,
+                  code: false,
+                  color: "default",
+                },
+                plain_text: name,
+                href: null,
+              },
+            ],
+          },
+        },
+      });
+    });
   } else {
     const db = await notion.databases.query({ database_id: config.notionDbId });
     const api = [];
